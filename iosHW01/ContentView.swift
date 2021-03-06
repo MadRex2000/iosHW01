5dc4e8eb73001f1cfa3326d32935249c39b3da53
MadRex2000 <spplkk113@gmail.com>
2021-03-06 19:35:52 +0800

    Initial Commit

---
+++ iosHW01/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iosHW01
+//
+//  Created by Rex Wu on 2021/3/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iosHW01/iosHW01App.swift
@@ -0,0 +1,17 @@
+//
+//  iosHW01App.swift
+//  iosHW01
+//
+//  Created by Rex Wu on 2021/3/6.
+//
+
+import SwiftUI
+
+@main
+struct iosHW01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
